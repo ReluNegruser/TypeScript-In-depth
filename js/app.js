7e4f3cfe939c0bdf6385afe33198db7ac0c01518
Relu Negruser <relu.negruser@thinslices.com>
2016-09-08 14:44:24 +0300

    Set up compiler and debugger

---
+++ js/app.js
@@ -0,0 +1,11 @@
+var HelloWorld = (function () {
+    function HelloWorld(message) {
+        this.message = message;
+    }
+    ;
+    return HelloWorld;
+}());
+var hello = new HelloWorld("Hello Typescript!");
+console.log(hello.message);
+console.log('Hello Visual Studio Code!');
+//# sourceMappingURL=app.js.map
